61a15a930b5629027113e5364d59c51fe5e6babd
luksari <luksari23@gmail.com>
2020-05-02 12:56:51 +0200

    Removed prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,7 +4,6 @@ module.exports = {
       'eslint:recommended',
       'plugin:react/recommended',
       'plugin:@typescript-eslint/recommended',
-      'prettier/@typescript-eslint',
     ],
     settings: {
       react: {
